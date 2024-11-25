cf52facae50328876859ddaa51a411f74e6d06b5
Uziel Saune <uzielelias07@gmail.com>
2024-11-24 21:42:17 -0500

    first commit

--- src/domain/datasource/dish.datasource.ts
+++ src/domain/datasource/dish.datasource.ts
@@ -1,9 +1,9 @@
 import { DishEntity } from "../entity/dish.entity";
 
 export abstract class DishDataSource {
-  abstract getDishes(): Promise<DishEntity[]>;
-  abstract findById(id: number): Promise<DishEntity[]>;
-  abstract logicalDeleteById(id: number): Promise<DishEntity>;
-  abstract updateDish(dish: DishEntity): Promise<DishEntity>;
-  abstract createDish(dish: DishEntity): Promise<DishEntity>;
+  abstract getDishes(): Promise<DishEntity[]> ;
+  abstract findById(id: number): Promise<DishEntity[]> ;
+  abstract logicalDeleteById(id: number): Promise<DishEntity> ;
+  abstract updateDish(dish: DishEntity): Promise<DishEntity> ;
+  abstract createDish(dish: DishEntity): Promise<DishEntity> ;
 }
--- src/domain/entity/dish.entity.ts
+++ src/domain/entity/dish.entity.ts
@@ -1,38 +1,39 @@
 import { DishInterface } from "../interface/dish.interface";
 
 export class DishEntity implements DishInterface {
-  dish_id?: number;
-  description?: string | null;
-  availability?: string | null;
-  name?: string | null;
+  dish_id?: number; 
+  description?: string | null; 
+  availability?: string | null; 
+  name?: string | null; 
   price?: number | null;
-  category?: string | null;
-  available?: boolean | null;
-  image_url?: string | null;
-  promotional_price?: number | null;
-  restaurant_id?: number | null;
-
-  constructor(
-    dish_id?: number,
-    description?: string | null,
-    availability?: string | null,
-    name?: string | null,
-    price?: number | null,
-    category?: string | null,
-    available?: boolean | null,
-    image_url?: string | null,
-    promotional_price?: number | null,
-    restaurant_id?: number | null
-  ) {
-    this.dish_id = dish_id;
-    this.description = description;
-    this.availability = availability;
-    this.name = name;
-    this.price = price;
-    this.category = category;
-    this.available = available;
-    this.image_url = image_url;
-    this.promotional_price = promotional_price;
-    this.restaurant_id = restaurant_id;
-  }
-}
+  category?: string | null; 
+  available?: boolean | null; 
+  image_url?: string | null; 
+  promotional_price?: number | null; 
+  restaurant_id?: number | null; 
+ 
+  constructor( 
+    dish_id?: number, 
+    description?: string | null, 
+    availability?: string | null, 
+    name?: string | null, 
+    price?: number | null, 
+    category?: string | null, 
+    available?: boolean | null, 
+    image_url?: string | null, 
+    promotional_price?: number | null, 
+    restaurant_id?: number | null 
+  ) { 
+    this.dish_id = dish_id; 
+    this.description = description; 
+    this.availability = availability; 
+    this.name = name; 
+    this.price = price; 
+    this.category = category; 
+    this.available = available; 
+    this.image_url = image_url; 
+    this.promotional_price = promotional_price; 
+    this.restaurant_id = restaurant_id; 
+  } 
+} 
+ 
